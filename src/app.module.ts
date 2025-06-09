965122516b8eea527390fd4e4639eddeab1b9438
itsAnkitAkm <itsamkankit@gmail.com>
2025-06-09 20:26:47 +0530

    issue in mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,25 +5,36 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FleetModule } from './fleet/fleet.module';
 import { UserModule } from './user/user.module';
+import { DroneModule } from './drone/drone.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ".env"
+      envFilePath: './.env',
     }),
     // MongooseModule.forRoot("mongodb://localhost/testLocation"),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGO_URI'),
-        retryAttempts: 5,
-        retryDelay: 1000,
-      }),
-      inject:[ConfigService]
+      useFactory: async (configService: ConfigService) => {
+        const uri = configService.get<string>('MONGO_URI');
+        console.log('🔍 MONGO_URI from configService:', uri);
+        if (!uri) {
+          throw new Error(
+            '❌ MONGO_URI is not defined in the environment variables.',
+          );
+        }
+        return {
+          uri,
+          retryAttempts: 5,
+          retryDelay: 1000,
+        };
+      },
+      inject: [ConfigService],
     }),
     FleetModule,
     UserModule,
+    DroneModule,
   ],
   controllers: [AppController],
   providers: [AppService],
